4d5c63441c19afadedafb3c58c6917159de3e79d
sameergit0 <sameerak1459@gmail.com>
2025-03-29 01:57:38 +0530

    Full Law Chatbot Project

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Law-Chatbot
+
+### Steps:
+
+### Step 1:
+1) Clone the repository
+https://github.com/sameergit0/Law-Chatbot.git
+
+### Step 2:
+2) Create and activate virtual environment
+python -m venv lawbot
+.\lawbot\Scripts\activate
+
+### Step 3:
+3) Install the requirements
+pip install -r requirements.txt
+
+
+### Step 4:
+4) Install embedding model from hugging face
+sentence-transformers/all-MiniLM-L6-v2 (This model will generate 384 dimensional embedding)
+
+### Step 5:
+5) Create pinecone account to store vector embeddings (here we set 384 dimensions)
+
+### Step 6:
+6) Create groq account to use llm (we are using llama3-8b-8192 model)
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Law Chatbot Project',
+    version='0.0.0',
+    description="A law Chatbot using Langchain",
+    author='Sameer Kureshi',
+    author_email='sameerak1459@gmail.com',
+    long_description=open('README.MD').read(),
+    url = 'https://github.com/sameergit0/Law-Chatbot.git',
+    packages=find_packages(),
+    install_requires =[
+        'sentence-transformers==4.0.1',
+        'langchain==0.3.21',
+        'langchain_community==0.3.20',
+        'langchain_experimental==0.3.4',
+        'langchain-pinecone==0.2.0',
+        'pinecone-client==5.0.1',
+        'langchain-groq==0.3.1',
+        'flask==3.1.0',
+        'pypdf==5.4.0',
+        'python-dotenv==1.1.0',
+        'pinecone[grpc]==6.0.2'
+    ],
+    classifiers=[                        
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Windows',
+    ],
+    python_requires='>=3.12.7'
+)
